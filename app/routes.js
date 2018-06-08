e57689da3912010edf3f311f51ccb17f2fc7850f
Francisco Serrano <fjserran@usc.edu>
2018-06-08 12:08:27 -0300

    removed unnecsary files of kickoff

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,8 @@
 // const controller = require('./controllers/controller');
-const logger = require('./logger'),
-  userFunctions = require('./controllers/userFunctions');
+const logger = require('./logger');
 
 exports.init = app => {
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
-  app.post('/users', [], userFunctions.createUser);
-  app.get('/', function(req, res) {
-    res.send('Get function to homepage.');
-  });
 };
