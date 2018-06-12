2594e8f4efd0cc0ca2f8f83378e192ddaa0632af
Francisco Serrano <fjserran@usc.edu>
2018-06-12 18:19:41 -0300

    Testing the User Test File

---
+++ test/user.js
@@ -0,0 +1,123 @@
+const chai = require('chai'),
+  dictum = require('dictum.js'),
+  server = require('./../app'),
+  should = chai.should();
+
+const chaiPost = (path, object) =>
+  chai
+    .request(server)
+    .post(path)
+    .send(object);
+
+describe('/users POST', () => {
+  it('should fail because first name is missing', done => {
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        lastName: 'lastName',
+        password: 'password',
+        email: 'email@wolox.com.ar'
+      })
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code');
+      })
+      .then(() => done());
+  });
+
+  /*
+
+  it('should fail because last name is missing', done => {
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        firstName: 'firstName',
+        password: 'password',
+        email: 'email@wolox.com.ar'
+      })
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code');
+      })
+      .then(() => done());
+  });
+
+  it('should fail because email is missing', done => {
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        firstName: 'firstName',
+        lastName: 'lastName',
+        password: 'password'
+      })
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code');
+      })
+      .then(() => done());
+  });
+
+  it('should fail because password is missing', done => {
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        firstName: 'firstName',
+        lastName: 'lastName',
+        email: 'email@wolox.com.ar'
+      })
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code');
+      })
+      .then(() => done());
+  });
+
+  it('should fail because password is too short/not alphanumeric', done => {
+    chai
+      .request(server)
+      .post('/users')
+      .send({
+        firstName: 'firstName',
+        lastName: 'lastName',
+        email: 'email@wolox.com.ar',
+        password: '234-'
+      })
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code');
+      })
+      .then(() => done());
+  });
+
+  it('should fail because email is already in use', done => {
+    const userObject = {
+      firstName: 'firstName',
+      lastName: 'lastName',
+      password: 'validpass',
+      email: 'email@wolox.com.ar'
+    };
+    chaiPost('/users', userObject)
+      .then(() => chaiPost('/users', userObject))
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code');
+      })
+      .then(() => done());
+  }); */
+});
