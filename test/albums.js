3573b15159031c836f2e48b1555a2ea91e3316b7
Francisco Serrano <fjserran@usc.edu>
2018-06-28 21:41:35 -0300

    Nock does not want to cooperate
    All tests written, only missing correct nock configuration to mock server response

--- test/albums.js
+++ test/albums.js
@@ -4,6 +4,7 @@ const chai = require('chai'),
   logger = require('../app/logger'),
   factory = require('./testFactory').factory,
   nock = require('nock'),
+  testNock = require('./nockResponses'),
   token = require('../app/services/tokenSessions'),
   should = chai.should(),
   expect = require('chai').expect;
@@ -140,5 +141,101 @@ describe('/albums/:id POST', () => {
         expect(err.response.body.internal_code).to.equal('invalid_token');
         done();
       });
+  });
+
+  before(() => {
+    nock.cleanAll();
+    const album4042 = nock('https://jsonplaceholder.typicode.com')
+      .get('/albums')
+      .reply(404);
+  });
+
+  it('should fail because external service is unavailable', done => {
+    factory.create('user').then(user => {
+      chai
+        .request(server)
+        .post('/albums/1')
+        .set(token.headerName, token.encode({ email: user.email }))
+        .catch(err => {
+          err.should.have.status(404);
+          err.response.body.should.have.property('message');
+          err.response.body.should.have.property('internal_code');
+          expect(err.response.body.internal_code).to.equal('fetch_error');
+          done();
+        });
+    });
+  });
+
+  after(() => {
+    nock.cleanAll();
+  });
+
+  before(() => {
+    const success2 = nock('https://jsonplaceholder.typicode.com')
+      .get('/albums')
+      .reply(200, [
+        {
+          userId: 1,
+          id: 1,
+          title: 'quidem molestiae enim'
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: 'sunt qui excepturi placeat culpa'
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: 'omnis laborum odio'
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: 'non esse culpa molestiae omnis sed optio'
+        },
+        {
+          userId: 1,
+          id: 5,
+          title: 'eaque aut omnis a'
+        }
+      ]);
+  });
+
+  /* it('should fail because user purchased the same album twice', done => {
+    factory.create('user').then(user => {
+      chai
+        .request(server)
+        .post('/albums/1')
+        .set(token.headerName, token.encode({ email: user.email }))
+        .then(() => {
+          chai
+            .request(server)
+            .post('/albums/1')
+            .set(token.headerName, token.encode({ email: user.email }))
+            .catch(err => {
+              err.should.have.status(404);
+              err.response.body.should.have.property('message');
+              err.response.body.should.have.property('internal_code');
+              expect(err.response.body.internal_code).to.equal('invalid_user');
+              expect(err.response.body.message).to.equal('User has already purchased this album.');
+              done();
+            });
+        });
+    });
+  }); */
+
+  it('should be successful', done => {
+    factory.create('user').then(user => {
+      chai
+        .request(server)
+        .post('/albums/1')
+        .set(token.headerName, token.encode({ email: user.email }))
+        .then(res => {
+          expect(res.status).to.equal(201);
+          dictum.chai(res);
+          done();
+        });
+    });
   });
 });
