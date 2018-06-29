ea4f6e1491920facd2686f81775a0db967bdf8af
Francisco Serrano <fjserran@usc.edu>
2018-06-29 13:31:51 -0300

    Requested changes

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,18 +1,13 @@
 'use strict';
 
 const logger = require('../logger'),
-  validation = require('./validation'),
-  tokens = require('./../services/tokenSessions'),
-  bcrypt = require('bcryptjs'),
+  requests = require('../services/requests'),
   errors = require('../errors'),
   rp = require('request-promise');
 
 exports.getAlbums = (req, res, next) => {
-  logger.info('GET Albums');
-  rp({
-    uri: 'https://jsonplaceholder.typicode.com/albums',
-    json: true
-  })
+  requests
+    .pingAlbums()
     .then(response => {
       res.send(response);
     })
